323573fb96c0b2fff5f028eb4da6525346afbad9
JOAO VITOR DA SILVEIRA SALABERRY <60037410075@senacrs.edu.br>
2026-04-07 16:25:31 -0300

    Codei em JS SOLOOOO (com ressalvas)

---
+++ template/busca/dashboard/script.js
@@ -0,0 +1,53 @@
+function mascara(i){
+   
+   var v = i.value;
+   
+   if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+      i.value = v.substring(0, v.length-1);
+      return;
+   }
+   
+   i.setAttribute("maxlength", "14");
+   if (v.length == 3 || v.length == 7) i.value += ".";
+   if (v.length == 11) i.value += "-";
+
+}
+
+function mascaraFone(event) {
+    var valor = document.getElementById("telefone").attributes[0].ownerElement['value'];
+    var retorno = valor.replace(/\D/g, "");
+    retorno = retorno.replace(/^0/, "");
+    if (retorno.length > 10) {
+      retorno = retorno.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (retorno.length > 5) {
+      if (retorno.length == 6 && event.code == "Backspace") { 
+        // necessário pois senão o "-" fica sempre voltando ao dar backspace
+        return; 
+      } 
+      retorno = retorno.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (retorno.length > 2) {
+      retorno = retorno.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+      if (retorno.length != 0) {
+        retorno = retorno.replace(/^(\d*)/, "($1");
+      }
+    }
+    document.getElementById("telefone").attributes[0].ownerElement['value'] = retorno;
+  }
+
+  function senhaLim(){
+    var senha = document.getElementById("senha").value
+    var divResp = document.getElementById("divResp")
+    if (senha.length == 0){
+      divResp.innerHTML = "Limpo"
+    }
+    else if(senha.length < 8){
+      divResp.innerHTML = "Senha com menos de 8 Digitos"
+    }
+    else if(senha.length > 8 && senha.match(/[A-Z]/)){
+      divResp.innerHTML = "Certinho"
+    }
+    else{
+      divResp.innerHTML = "Senha com mais de 8 digitos sem Letra maiúscula"
+    }
+  }

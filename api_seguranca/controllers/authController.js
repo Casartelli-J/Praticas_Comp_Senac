480fc01c453ba78fafd5a8c9d62971e5765ffbbf
ROBERTO DOS SANTOS OLIVEIRA <01119704073@senacrs.edu.br>
2026-05-13 16:28:31 -0300

    Json Web Token

---
+++ api_seguranca/controllers/authController.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import db from "../config/database.js";
+
+export const login = async (req, res) => {
+    const {email, senha} = req.body;
+    const users = "SELECT * from usuarios WHERE email = ?";
+    
+    const [busca] = await db.query(users, [email]);
+    const user = busca[0]
+    
+    const comparaSenha = await bcrypt.compare(senha, user["senha"])
+    console.log(comparaSenha)
+
+}
